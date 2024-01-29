077dc05a17540025d1decc8d7e5d6eb443d20df1
azmiodeh <129420533+azmiodeh@users.noreply.github.com>
2024-01-29 12:40:10 +0200

    added Dice class

---
+++ src/main/java/com/example/snakesandladdersviper/model/Dice.java
@@ -0,0 +1,12 @@
+package com.example.snakesandladdersviper.model;
+
+public class Dice {
+    public Dice() {
+        // Initialize dice attributes here
+    }
+
+    public int roll() {
+        /* Simulate rolling a dice and return the result */
+        return 0;
+    }
+}
