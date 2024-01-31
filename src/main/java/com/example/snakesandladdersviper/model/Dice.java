1b32f29ae3c63183862f9daa432e6028857c48d3
7usney1802 <husney1999@gmail.com>
2024-01-31 14:32:15 +0200

    Dice Edited

--- src/main/java/com/example/snakesandladdersviper/model/Dice.java
+++ src/main/java/com/example/snakesandladdersviper/model/Dice.java
@@ -1,12 +1,24 @@
 package com.example.snakesandladdersviper.Model;
+import java.util.Random;
 
 public class Dice {
-    public Dice() {
-        // Initialize dice attributes here
+    private final Random random;
+    private final int maxRoll;
+    private final double questionProbability;
+
+    public Dice(int maxRoll, double questionProbability) {
+        this.random = new Random();
+        this.maxRoll = maxRoll;
+        this.questionProbability = questionProbability;
     }
 
     public int roll() {
-        /* Simulate rolling a dice and return the result */
-        return 0;
+        // Roll a number between 0 and maxRoll (inclusive)
+        return random.nextInt(maxRoll + 1);
     }
+
+    public boolean shouldAskQuestion() {
+        // Determine if a question should be asked based on probability
+        return random.nextDouble() < questionProbability;
+}
 }
