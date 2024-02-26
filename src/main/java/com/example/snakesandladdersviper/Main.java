66dcb3134448bfef20d8025402c4cec25a96e2b1
Sami799 <102754089+Sami799@users.noreply.github.com>
2024-02-26 16:57:37 +0800

    Revert "full screens"
    This reverts commit a785b1d83073376364a85890d4fd3bcfc9fd1e70.

--- src/main/java/com/example/snakesandladdersviper/Main.java
+++ src/main/java/com/example/snakesandladdersviper/Main.java
@@ -1,6 +1,5 @@
 package com.example.snakesandladdersviper;
 
-import com.example.snakesandladdersviper.Controller.SceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,32 +9,18 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    private Stage primaryStage; // Add a class member to hold the primary stage
-
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        // Create an instance of your SceneController, passing the primaryStage to its constructor
-        SceneController sceneController = new SceneController(primaryStage);
-
-        // Now, you can use sceneController to switch scenes as needed
-        // For the initial scene, you might want to do something like this:
+    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        primaryStage.setTitle("Snakes and Ladders");
-        primaryStage.setScene(scene);
-        primaryStage.setFullScreen(true);
-        primaryStage.show();
-    }
+        stage.setTitle("Snakes and Ladders");
+        stage.setScene(scene);
 
-    public void switchScene(String fxmlFile) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
-        try {
-            Scene newScene = new Scene(fxmlLoader.load());
-            primaryStage.setScene(newScene);
-            // Fullscreen is maintained, as per the listener in the start method
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Show the stage first
+        stage.show();
+
+        // Then set full-screen mode
+        stage.setFullScreen(true);
     }
 
     public static void main(String[] args) {
